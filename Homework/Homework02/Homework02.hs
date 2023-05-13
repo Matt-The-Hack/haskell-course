b608756417252e183ec0b1e05357ec2f92238c96
Matt Carlson <MattTheHack@protonmail.com>
2023-05-13 18:22:35 -0400

    add Changes

--- Homework/Homework02/Homework02.hs
+++ Homework/Homework02/Homework02.hs
@@ -3,9 +3,12 @@
 -- Add the type signatures for the functions below and then remove the comments and try to compile.
 -- (Use the types presented in the lecture.)
 
--- f1 x y z = x ** (y/z)
+   --Can use double as well
+   f1 :: Float -> Float -> Float -> Float
+   f1 x y z = x ** (y/z)
 
--- f2 x y z = sqrt (x/y - z)
+   f2 :: Float -> Float -> Float -> Float
+   f2 x y z = sqrt (x/y - z)
 
 -- f3 x y = [x == True] ++ [y]
 
